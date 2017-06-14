e0efb0ae9efa4b9c302ba289e685aca1a49c516c
RyanGan <ryan.william.gan@gmail.com>
2017-06-14 15:58:00 -0600

    updated presented values a bit.

--- ui.R
+++ ui.R
@@ -1,29 +1,48 @@
+# ------------------------------------------------------------------------------
+# Title: UI script for smoke forecaster shiny app
+# Author: Ryan Gan and Steve Brey
+# Date Created: 6/17/17
+# R Version 3.3.3 
+# ------------------------------------------------------------------------------
+
+# load libraries ----
 library(shiny)
 library(leaflet)
 library(raster)
 
-max(smk_brick)
+# read net cdf file of forecasts from 06/08/2017 ----
+# brick or stack works
+smk_brick <- brick("./smoke_dispersion_v2.nc")
+# find the max range
+max_pm <- max(summary(smk_brick)[5,])
+
+smk_brick <- calc(smk_brick, fun=function(x){
+  x[x > 200] <- 200; return(x)
+})
+
 # define color gradient for layer ----
-pal <- colorNumeric(c("#F0F2F0", "#000c40"), domain = c(0,600), 
+pal2 <- colorBin(c("#F0F2F0", "#000c40"), domain = c(0,max_pm), bin = c(0:10),
+  pretty=T, na.color = "transparent")
+
+pal2
+
+pal <- colorNumeric(c("#F0F2F0", "#000c40"), domain = c(0,200),
                     na.color = "transparent")
 
-# read net cdf file of forecasts from 06/08/2017
-smk_brick <- stack("./smoke_dispersion_v2.nc")
+# I need to convert date and time
+min_date
 
-r_test <- smk_brick[[180]]
-
-# smaller brick
-small_brick <- smk_brick[[1:10]]
-
-
+# set up shiny layout
 ui <- fluidPage(
   leafletOutput("map"),
   sliderInput(inputId = "time", 
-              label = "Date", 
-              min =1, max = 10,
-              value = 1, step = 1)
+              label = "Date:Time", 
+              min =1, max = 192,
+              value = 0, step = 1)
 )
 
+
+# server section that will eventually go in it's own script
 server <- function(input, output, session) {
   
   # add base leaflet map
@@ -32,27 +51,22 @@ server <- function(input, output, session) {
       # call map layer
       addTiles() %>% 
       # set bounds of map
-      fitBounds(lng1=-100, lat1=50, lng2=-90, lat2=25) 
+      fitBounds(lng1=-100, lat1=50, lng2=-90, lat2=25) %>% 
+      addLegend(pal=pal, values=c(0,max_pm), title = "Smoke ug/m^3")
   map  
     
   }) #
   
-  # add interactive raster brick
+  # add interactive raster layer
   observeEvent(input$time,{
-    
-    # reactive raster brick
-    print(input$time %in% names(small_brick))
     
     time_index_string <- paste0("X", input$time)
     index <- as.numeric(input$time)
-    r <- reactive({small_brick[[index]]})
-    #r <- raster(xmn=-2.8, xmx=-2.79, ymn=54.04, ymx=54.05, nrows=30, ncols=30) 
+    r <- reactive({smk_brick[[index]]})
     
     leafletProxy(mapId="map") %>%
       clearImages() %>%
-     
-      addRasterImage(r_test, colors = pal, opacity = 0.5, project = T) #%>%
-      #addLegend(pal=pal, values=r, title = "Smoke ug/m^3")
+      addRasterImage(r(), colors = pal, opacity = 0.3, project = F) 
     
     })
 }
