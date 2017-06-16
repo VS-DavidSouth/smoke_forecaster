a57108d5f94176a31db70e04c1d297c846545440
RyanGan <ryan.william.gan@gmail.com>
2017-06-16 10:16:40 -0600

    fixed some things.

--- ui.R
+++ ui.R
@@ -13,47 +13,43 @@ library(raster)
 # read net cdf file of forecasts from 06/08/2017 ----
 # brick or stack works
 smk_brick <- brick("./smoke_dispersion_v2.nc")
+
+# set upper bound to 160 and anything lower to NA for nicer raster presentation
+smk_brick <- calc(smk_brick, fun=function(x){
+  x[x > 160] <- 160;
+  x[x < 5] <- NA; return(x)
+})
+
+smk_brick@legend
 # find the max range
 max_pm <- max(summary(smk_brick)[5,])
 
-smk_brick <- calc(smk_brick, fun=function(x){
-  x[x > 200] <- 200; return(x)
-})
-
 # define color gradient for layer ----
-pal2 <- colorBin(c("#F0F2F0", "#000c40"), domain = c(0,max_pm), bin = c(0:10),
-  pretty=T, na.color = "transparent")
-
-pal2
-
-pal <- colorNumeric(c("#F0F2F0", "#000c40"), domain = c(0,200),
+pal <- colorNumeric(c("#F0F2F0", "#000c40"), domain = c(0,160),
                     na.color = "transparent")
 
-# I need to convert date and time
-min_date
+# convert date a time
+smk_brick
 
 # set up shiny layout
 ui <- fluidPage(
   leafletOutput("map"),
   sliderInput(inputId = "time", 
-              label = "Date:Time", 
-              min =1, max = 192,
-              value = 0, step = 1)
+    label = "Date:Time", min =1, max = 192, value = 0, step = 1,
+    animate = animationOptions(interval=200, loop=T))
 )
-
 
 # server section that will eventually go in it's own script
 server <- function(input, output, session) {
   
   # add base leaflet map
   output$map <- renderLeaflet({
-   map <-  leaflet() %>% 
-      # call map layer
-      addTiles() %>% 
-      # set bounds of map
-      fitBounds(lng1=-100, lat1=50, lng2=-90, lat2=25) %>% 
-      addLegend(pal=pal, values=c(0,max_pm), title = "Smoke ug/m^3")
-  map  
+    leaflet() %>% 
+    # call map layer
+    addTiles() %>% 
+    # set bounds of map
+    fitBounds(lng1=-100, lat1=50, lng2=-90, lat2=25) %>% 
+    addLegend(pal=pal, values=c(0,max_pm), title = "Smoke ug/m^3")
     
   }) #
   
@@ -66,7 +62,7 @@ server <- function(input, output, session) {
     
     leafletProxy(mapId="map") %>%
       clearImages() %>%
-      addRasterImage(r(), colors = pal, opacity = 0.3, project = F) 
+      addRasterImage(r(), colors = pal, opacity = 0.7, project = F) 
     
     })
 }
