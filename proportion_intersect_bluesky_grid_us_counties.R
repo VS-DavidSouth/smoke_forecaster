afdb049d8f68913c2f06e9579ce877a27b8aad73
RyanGan <ryan.william.gan@gmail.com>
2017-06-28 16:34:13 -0600

    slowly setting projections to be the same.

--- proportion_intersect_bluesky_grid_us_counties.R
+++ proportion_intersect_bluesky_grid_us_counties.R
@@ -10,6 +10,9 @@
 library(raster)
 library(sf)
 library(tidyverse)
+library(rgdal)
+library(rgeos)
+library(sp)
 
 # load raster brick ----
 nc_path <- "smk_stack_raster.nc"
@@ -22,6 +25,33 @@ bluesky_grid <- rasterToPolygons(smk_forecast[[1]])
 plot(bluesky_grid)
 bluesky_grid
 
+# get a bounding box of the bluesky grid
+bbox <- extent(bluesky_grid)
+# get projection
+wgs84 <- proj4string(bluesky_grid)
+
 # get ids of cells
 test <- sapply(slot(bluesky_grid, "polygons"), function(x) slot(x, "ID"))
 head(test)
+
+
+# load county shapefile
+us_county <- readOGR(dsn = "./tl_2016_us_county/tl_2016_us_county.shp",
+                     layer = "tl_2016_us_county")
+
+# set projection of US shape to same as bluesky
+us_county <- spTransform(us_county, CRS(wgs84))
+
+proj4string(us_county)
+proj4string(bluesky_grid)
+# subset counties to only those in bluesky extent
+gClip <- function(shp, bb){
+  if(class(bb) == "matrix") b_poly <- as(extent(as.vector(t(bb))), 
+                                         "SpatialPolygons")
+  else b_poly <- as(extent(bb), "SpatialPolygons")
+  gIntersection(shp, b_poly, byid = TRUE)
+}
+
+# warning produced by gclip function, but seems okay
+us_county <- gClip(us_county, bbox)
+plot(us_county)
