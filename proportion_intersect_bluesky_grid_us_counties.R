de57110b44240d22bf046f87ec35cc098cd3f6a9
RyanGan <ryan.william.gan@gmail.com>
2017-06-28 16:08:51 -0600

    adding stuff

--- proportion_intersect_bluesky_grid_us_counties.R
+++ proportion_intersect_bluesky_grid_us_counties.R
@@ -9,6 +9,7 @@
 # load libraries ----
 library(raster)
 library(sf)
+library(tidyverse)
 
 # load raster brick ----
 nc_path <- "smk_stack_raster.nc"
@@ -17,4 +18,10 @@ nc_path <- "smk_stack_raster.nc"
 smk_forecast <- brick(nc_path)
 
 # subset to one layer and plot grid
-bluesky_grid <- smk_forecast[[1]]
+bluesky_grid <- rasterToPolygons(smk_forecast[[1]])
+plot(bluesky_grid)
+bluesky_grid
+
+# get ids of cells
+test <- sapply(slot(bluesky_grid, "polygons"), function(x) slot(x, "ID"))
+head(test)
