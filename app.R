eda2154fb6f4fd90ba7d3da742222efd8db7a336
RyanGan <ryan.william.gan@gmail.com>
2017-07-13 14:04:21 -0400

    I've started to modify code to use a shapefile instead of raster to see if that works on the server. Still trying to figure out how to make the days reactive.

--- app.R
+++ app.R
@@ -12,66 +12,29 @@
 # load libraries ---------------------------------------------------------------
 library(shiny)
 library(leaflet)
-library(raster)
-#library(ncdf4)
+library(rgdal) # read shapefile
 
-# define direct path to file
-#nc_path <- "/srv/shiny-server/smoke_forecaster/smk_stack_raster.nc"
- nc_path <- "smk_stack_raster.nc"
+# define direct path to polygon file
+#poly_path <- "/srv/shiny-server/smoke_forecaster/smk_stack_raster.nc"
+poly_path <- "smk_poly"
 
-# using single raster layer of next day average
-# note july 3rd 2017: brick is causing shiny app to not run
-# error message: ERROR: "R_nc4_open" not available for .C() for package "ncdf4"
-# perhaps it doesn't like; also asked tyson to install gdal on server which may help
-smk_forecast <- brick(nc_path)
+# read in shapefile
+smk_forecast <- readOGR(dsn = poly_path, layer = poly_path)
 
+# set upper bound to 200
+smk_forecast[smk_forecast$layer_1 >= 250, ] <- 249
+smk_forecast[smk_forecast$layer_2 >= 250, ] <- 249
 
-# set upper bound to 160 and anything lower to NA for nicer raster presentation
- smk_forecast <- calc(smk_forecast, fun=function(x){
-   x[x >= 200] <- 199;
-   x[x < 5] <- NA; 
-   return(x)
- })
-
-
-# define color gradient for layer ----
-pal <- colorNumeric(c("#F0F2F0", "#000c40"), domain = c(0,200),
+# define color bin for layer ----
+# going with a bin since it will be easier to handle extreme colors
+bin <- c(0, 5, 10, 25, 50, 100, 250)
+pal <- colorBin(c("#F0F2F0", "#000c40"), domain = c(0,250), bins = bin,
                     na.color = "transparent")
 
 # identify today's date
 todays_date <- format(as.Date(Sys.Date()), "%B %d, %Y")
 # identify tomorrows date
 date_tomorrow <- format(as.Date(Sys.Date()+1), "%B %d, %Y")
- 
-# server section ----
-server <- (function(input, output){
-  
-  # add base leaflet map
-  output$map <- renderLeaflet({
-    leaflet() %>% 
-      # call map layer
-      addTiles() %>% 
-      # set bounds of map
-      fitBounds(lng1=-100, lat1=50, lng2=-90, lat2=25) %>% 
-      addLegend(pal=pal, values=c(0, 200), title = "Smoke ug/m^3",
-                position = "bottomright")
-  })# end base leaflet
-  
-  # add interactive raster layer
-  observeEvent(input$date_smoke,{
-   # reactive raster layer
-    index <- as.numeric(input$date_smoke)
-    
-    r <- reactive({smk_forecast[[index]]})
-    
-    # call proxy map
-    leafletProxy(mapId="map") %>%
-      clearImages() %>%
-      addRasterImage(r(), colors = pal, opacity = 0.8, project = T) 
-
-  }) # end reactive layer
-    
-}) # end server function
 
 # set up shiny layout
 ui <- bootstrapPage(
@@ -83,12 +46,71 @@ ui <- bootstrapPage(
   # adding a radio button for today's or tomorrow's forecast
   absolutePanel(top = 75, right = 25,
                 radioButtons("date_smoke", label = h3("Date of Smoke"), 
-                              choices = list("Today"=1, "Tomorrow"=2),
-                              selected = 1), hr(), 
-                              fluidRow(column(2,verbatimTextOutput("value"))))
+                             choices = list("Today"="layer_1", 
+                                            "Tomorrow"="layer_2"),
+                             selected = "layer_1"), hr(), 
+                fluidRow(column(2,verbatimTextOutput("value"))))
   # note for the radio button; having trouble listing date based on system time
-  ) # end UI function
+) # end UI function
+
+# server section ----
+server <- (function(input, output){
+  
+  # add base leaflet map
+  output$map <- renderLeaflet({
+    leaflet() %>% 
+      # call map layer
+      addTiles() %>% 
+      # set bounds of map
+      fitBounds(lng1=-100, lat1=50, lng2=-90, lat2=25) %>% 
+      addLegend(pal=pal, values=c(0, 250), title = "Smoke ug/m^3",
+                position = "bottomright") %>% 
+      addPolygons(data = smk_forecast, 
+                  color = "tranparent", 
+                  fillColor = ~pal(smk_forecast$layer_2), 
+                  weight = 1, smoothFactor = 2,
+                  fillOpacity = 0.5,
+                  # add highlight option
+                  highlight = highlightOptions(
+                    weight = 5, color = "blue", bringToFront = T, fillOpacity = 0.8),
+                  # add smoke pm values
+                  label = pm_label
+      )
+    
+  })# end base leaflet
+  
+  # add interactive raster layer
+  observeEvent(input$date_smoke,{
+   # set index as 1 or 2 for easier index
+    poly_name <- as.numeric(input$date_smoke)
+   # reactive polygon layer
+    poly <- reactive({smk_forecast[[index]]})
+    
+    # define smoke label values
+    pm_label <- sprintf("%g ug/m^3 of smoke", round(smk_forecast$layer_2, 1)) %>% 
+      lapply(htmltools::HTML)
+    
+    # call proxy map
+    leafletProxy(mapId="map") %>%
+      #removeShape() %>%
+      # add polygon 
+      addPolygons(data = poly(), # data = smk_forecast, 
+        color = "tranparent", 
+        #fillColor = ~pal(smk_forecast$layer_2), 
+        weight = 1, smoothFactor = 2,
+        fillOpacity = 0.5,
+        # add highlight option
+        highlight = highlightOptions(
+          weight = 5, color = "blue", bringToFront = T, fillOpacity = 0.8),
+        # add smoke pm values
+        label = pm_label
+      )
+  }) # end reactive layer
+    
+}) # end server function
+
 
 # launch shiny app (this is necessary for running on server)
 shinyApp(ui = ui, server = server)
+
 
