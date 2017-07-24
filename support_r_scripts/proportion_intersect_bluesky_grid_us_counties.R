c6bcbb3e2396a5d57bba9f0b1e7217b73c24f289
RyanGan <ryan.william.gan@gmail.com>
2017-07-24 10:20:24 -0400

    trying to calculate proportion intersect.

--- support_r_scripts/proportion_intersect_bluesky_grid_us_counties.R
+++ support_r_scripts/proportion_intersect_bluesky_grid_us_counties.R
@@ -18,21 +18,30 @@ poly_layer <- "bluesky_grid"
 
 # read grid polygon
 bluesky_grid <- readOGR(dsn = poly_path, layer = poly_layer)
-bluesky_grid
 
-# load US county grid ----
+# assign a grid id to each bluesky cell (i could do this in the other step)
+bluesky_grid$id <- as.character(seq(1:94068))
+# save bluesky grid ids as a vector (not sure it needs to be character or numeric)
+bs_id <- sort(bluesky_grid@data$id)
+# reordering variables just because
+bluesky_grid <- bluesky_grid[,2:1]
+
+# load US county grid ----------------------------------------------------------
 co_path <- "./data/us_county"
 co_layer <- "us_county"
 
 # read county polygons
 us_county <- readOGR(dsn = co_path, layer = co_layer)
 
-# calculating overlap between the bluesky grid cells and counties ----
-# expecting a large matrix of 3108 counties by 94068 grid cells
 # save county ids (n = 3108) as a vector
 county_id <- as.character(sort(us_county@data$COUNTYFP))
-# assign a grid id to each bluesky cell (i could do this in the other step)
-# save bluesky grid ids as a vector
-bs_id <- as.character(sort(bluesky_grid@data$id))
 
+# Parallel computing of intersections ------------------------------------------
+# calculating overlap between the bluesky grid cells and counties 
+# creating empty large matrix of 3108 counties by 94068 grid cells
+# bs_county_proportion <- matrix(nrow=3108, ncol=94068, byrow=T,
+#   dimnames = list(county_id, bs_id))
 
+# try a much smaller matrix to see if it works
+plot(us_county)
+plot(bluesky_grid, add=T)
