66210a0de131960c759b5ad5bbcc5b10553dce75
RyanGan <ryan.william.gan@gmail.com>
2017-09-13 08:52:35 -0700

    updated date again. still needs work.

--- support_r_scripts/daily_bluesky_download.R
+++ support_r_scripts/daily_bluesky_download.R
@@ -154,7 +154,7 @@ same_day_smk <- smk_brick[[1:29]]
 # create raster layer of mean value
 same_day_mean_smk <- mean(same_day_smk)
 # extract the date without timestamp (taking element date 29 frome 1:29)
-same_day_date  <- as.numeric(substring(smk_brick@data@names, 2))[29]
+same_day_date  <- as.numeric(substring(smk_brick@data@names, 2))[20]
 # assign date time stamp in a format of month_day_year to bind with name
 same_day_date <- format(as.POSIXct(same_day_date, origin="1970-1-1", tz="GMT"),
                     format = "%b %d %Y")
@@ -165,7 +165,7 @@ next_day_smk <- smk_brick[[30:54]]
 # create raster layer of daily mean value
 next_day_mean_smk <- mean(next_day_smk)
 # extract next day's date
-next_day_date  <- as.numeric(substring(smk_brick@data@names, 2))[54]
+next_day_date  <- as.numeric(substring(smk_brick@data@names, 2))[45]
 # assign date time stamp in a format of month_day_year to bind with name
 next_day_date <- format(as.POSIXct(next_day_date, origin="1970-1-1", tz="GMT"),
                         format = "%b %d %Y")
