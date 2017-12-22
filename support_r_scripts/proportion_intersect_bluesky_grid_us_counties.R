bca5f7fe729a0914ed9d1e61ceb91d32e829193d
RyanGan <ryan.william.gan@gmail.com>
2017-12-21 20:06:22 -0500

    updated a lot of files and making some tutorials.

--- support_r_scripts/proportion_intersect_bluesky_grid_us_counties.R
+++ support_r_scripts/proportion_intersect_bluesky_grid_us_counties.R
@@ -12,8 +12,8 @@
 library(tidyverse)
 library(sf)
 # parallel compuation
-library(doParallel)
-library(parallel)
+# library(doParallel)
+# library(parallel)
 
 # read in bluesky grid sf and us county sf --------
 # define relative path to polygon file
@@ -28,7 +28,9 @@ bluesky_grid <- st_read(dsn = poly_path, layer = poly_layer)
 # the bluesky grid does not have an ID so we will assign each cell a number
 bluesky_grid$id <- as.numeric(seq(1:94068))
 # read county polygons
-us_county <- st_read(dsn = co_path, layer = co_layer)
+us_county <- st_read(dsn = co_path, layer = co_layer) %>% 
+  # making a FIPS ID that I'll use later
+  mutate(FIPS = paste0(STATEFP, COUNTYFP))
 
 # create empty tibble 
 # tibble of just the grid_id to join data too
@@ -38,58 +40,77 @@ prop_int_tibble <- bluesky_grid$id %>%
 
 # set up parallel computing ----
 # find number of processing nodes (cores)
-cores <- detectCores() # using all the cores
-# make cluster/node
-cl <- makeCluster(cores)
-# register do parallel cluster
-registerDoParallel(cl)
-
-# load packages on each processor of the node/cluster
-clusterCall(cl, function() c(library(tidyverse), library(sf)))
-
-# export global sf objects and empty tibble to each core
-clusterExport(cl, c("bluesky_grid", "us_county", "prop_int_tibble"), 
-              envir = .GlobalEnv)
+# cores <- detectCores() # using all the cores
+# # make cluster/node
+# cl <- makeCluster(cores)
+# # register do parallel cluster
+# registerDoParallel(cl)
+# 
+# # load packages on each processor of the node/cluster
+# clusterCall(cl, function() c(library(tidyverse), library(sf)))
+# 
+# # export global sf objects and empty tibble to each core
+# clusterExport(cl, c("bluesky_grid", "us_county", "prop_int_tibble"), 
+#               envir = .GlobalEnv)
 
 
 # for loop ----
 # start time 
 start_time <- Sys.time()
 
-foreach(i=1:length(us_county$FIPS)) %dopar% {
+for(i in 1:length(us_county$FIPS)){
   # subset county to find intersect
   county <- slice(us_county, i)
   # extract fips number for variable name
   fips_id <- paste0("fips_", county$FIPS)
   # subset grid cells that touch any part of the county
-  grid <- cali_grid[county,]
-  # output grid IDs
-  grid_id <- grid$id
+  grid <- bluesky_grid[county,]
   # subset the intersected polygon
-  inter_poly <- st_intersection(st_geometry(grid),st_geometry(county))
+  inter_poly <- st_intersection(grid, county) %>% 
+    # filter only to polygon or multipolygon type 
+    # to avoid errors with point or line types
+    filter(st_is(., c("POLYGON", "MULTIPOLYGON"))==T)
+  # filter grid ids to only grids in the inter_poly object
+  grid2 <- grid %>% filter(id %in% inter_poly$id) 
   # find proportion intersect with original grid
-  prop_int <- as.numeric(st_area(inter_poly)/st_area(grid))
+  prop_int <- as.numeric(st_area(inter_poly)/st_area(grid2))
+  # subset grid id
+  grid_id <- grid2$id
   # make a tibble
-  prop_int_tibble <- tibble(grid_id, prop_int) %>% 
-    set_names(c("grid_id", fips_id)) %>% 
-    right_join(prop_int_tibble, by = "grid_id")
+  county_grid_int <- tibble(grid_id, prop_int) %>% 
+    set_names(c("grid_id", fips_id))
+  # join with full tibble
+  prop_int_tibble <- prop_int_tibble %>% 
+    left_join(county_grid_int, by = "grid_id")
 } # end loop
 
 # stop cluster
-stopCluster(cl)
+#stopCluster(cl)
 
 stop_time <- Sys.time()
 compute_time <- stop_time - start_time
 # run time
 compute_time
 
-# that was fast enough and easier to follow
-summary(prop_int_tibble)
-
 # set missing NA values to 0
 bluesky_county_pi <- prop_int_tibble %>% 
   mutate_all(funs(replace(., is.na(.), 0)))
 
+# output some counties to check intersect
+cali_county <- bluesky_county_pi %>% 
+  select(grid_id, contains("fips_06"))
+
+# checking LA county against my markdown file.
+la_county <- cali_county %>% 
+  select(grid_id, fips_06037) %>% 
+  filter(fips_06037 > 0)
+
 # save final proportion intersect ----
 save_path <- "./data/bluesky_county_prop_intersect.csv"
 write_csv(bluesky_county_pi, save_path)
+
+# save california fips for checking
+save_cali <- "./data/cali_county_pi.csv"
+write_csv(cali_county, save_cali)
+
+
