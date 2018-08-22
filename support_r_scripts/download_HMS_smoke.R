bc0ca8cc18a807ab38de200fe3250911b4d1491d
Steven <stevenjoelbrey@gmail.com>
2018-08-22 16:22:00 -0600

    time string is reformatted.

--- support_r_scripts/download_HMS_smoke.R
+++ support_r_scripts/download_HMS_smoke.R
@@ -6,6 +6,8 @@ if(length(args)>0){
   machine_name <- "local"
 }
 
+library(rgdal)
+
 if(machine_name == "salix"){
   
   setwd("/srv/www/rgan/smoke_forecaster/")
@@ -27,5 +29,21 @@ for (ext in extensions){
   url <- paste0(urlBase, "latest_smoke", ext)
   download.file(url=url, destfile=destFile)
 }
+
+# Read in for formating 
+latest_smoke <- rgdal::readOGR(dsn=paste0(home_path, "data/HMS"), 
+                               layer="latest_smoke")
+
+# Format the annoying time format YYYDD HH
+latest_smoke$Start <- as.POSIXct(latest_smoke$Start, format = "%Y%d %H", tz="UTC")
+latest_smoke$End <- as.POSIXct(latest_smoke$End, format = "%Y%d %H", tz="UTC")
+
+# rewrite the file
+rgdal::writeOGR(obj = latest_smoke, 
+                dsn = paste0(home_path,"data/HMS"), 
+                layer = "latest_smoke", 
+                driver = "ESRI Shapefile", 
+                overwrite_layer = T)
+
 # TODO: Consider sharing this information with the user on the site. 
 print(paste("HMS smoke plumes updated at:", Sys.time()))
