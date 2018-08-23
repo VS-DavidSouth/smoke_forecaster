0fe0d8a18e489d30f20dc032adb6fb3ad80418d2
Steven <stevenjoelbrey@gmail.com>
2018-08-23 09:08:29 -0600

    Download script changed to handle when there are no plumes analysed

--- support_r_scripts/download_HMS_smoke.R
+++ support_r_scripts/download_HMS_smoke.R
@@ -6,6 +6,14 @@ if(length(args)>0){
   machine_name <- "local"
 }
 
+################################################################################
+# Description
+################################################################################
+# This script downloads the latest HMS smoke plume analysis available from 
+# the archive at https://www.ospo.noaa.gov/Products/land/hms.html
+# If no new analysis is available, i.e. no polygons in the file linked online,
+# the latest file is not used, and instead, the older polygons are retained. 
+
 library(rgdal)
 
 if(machine_name == "salix"){
@@ -27,23 +35,40 @@ extensions <- c(".dbf", ".shp", ".shx")
 for (ext in extensions){
   destFile <- paste0("./data/HMS/latest_smoke", ext)
   url <- paste0(urlBase, "latest_smoke", ext)
-  download.file(url=url, destfile=destFile)
+  download.file(url=url, destfile=destFile, cacheOK=F)
 }
 
-# Read in for formating 
-latest_smoke <- rgdal::readOGR(dsn=paste0(home_path, "data/HMS"), 
-                               layer="latest_smoke")
+# Read in for formatting
+# NOTE: Sometimes this file contains no features. When that is the case, do not
+# NOTE: write a new display file, as it would be blank and throw an error. 
+try_error <- try(
+  latest_smoke <- rgdal::readOGR(dsn=paste0(home_path, "data/HMS"), 
+                                 layer="latest_smoke"),
+  silent=T
+)
 
-# Format the annoying time format YYYDD HH
-latest_smoke$Start <- as.POSIXct(latest_smoke$Start, format = "%Y%d %H", tz="UTC")
-latest_smoke$End <- as.POSIXct(latest_smoke$End, format = "%Y%d %H", tz="UTC")
 
-# rewrite the file
-rgdal::writeOGR(obj = latest_smoke, 
-                dsn = paste0(home_path,"data/HMS"), 
-                layer = "latest_smoke", 
-                driver = "ESRI Shapefile", 
-                overwrite_layer = T)
+if(class(try_error) == "try-error"){
+  
+  print("No polygon features in the latest file. Not updating for now.")
+  
+} else{
+  
+  print("Features in the latest smoke file. Updating the existing.")
+  
+  # # Format the annoying time format YYYDD HH
+  # latest_smoke$Start <- as.POSIXct(latest_smoke$Start, format = "%Y%d %H", tz="UTC")
+  # latest_smoke$End <- as.POSIXct(latest_smoke$End, format = "%Y%d %H", tz="UTC")
+  
+  # rewrite the file
+  rgdal::writeOGR(obj = latest_smoke, 
+                  dsn = paste0(home_path,"data/HMS"), 
+                  layer = "latest_smoke_display", 
+                  driver = "ESRI Shapefile", 
+                  overwrite_layer = T)
+  
+}
 
 # TODO: Consider sharing this information with the user on the site. 
 print(paste("HMS smoke plumes updated at:", Sys.time()))
+print("Script run successfully.")
